cab395f89c806840e8a2ff6290838e138cbf8997
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-21 16:20:54 +0900

    🗂️ :: BaseEntity

---
+++ CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/common/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.example.cqrswrite.common.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTimeEntity extends BaseUUIDEntity {
+
+    @CreatedDate
+    @Column(columnDefinition = "DATETIME(6)", updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/common/entity/BaseUUIDEntity.java
@@ -0,0 +1,25 @@
+package com.example.cqrswrite.common.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseUUIDEntity {
+
+    @Id
+    @GeneratedValue(generator = "timeBasedUUIDGenerator")
+    @GenericGenerator(name = "timeBasedUUIDGenerator", strategy = "com.example.hexagonalfeed.common.entity.TimeBasedUUIDGenerator")
+    @Column(columnDefinition = "BINARY(16)", nullable = false)
+    private UUID id;
+}
