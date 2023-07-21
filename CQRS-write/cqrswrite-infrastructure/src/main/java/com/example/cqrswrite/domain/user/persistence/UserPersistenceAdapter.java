dc49bdd94a1db93eadf4c35baed9c5d3d8e158ba
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-21 16:24:53 +0900

    🦷 :: 이름 변경

--- CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/domain/user/persistence/UserPersistenceAdapter.java
+++ CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/domain/user/persistence/UserPersistenceAdapter.java
@@ -3,13 +3,13 @@ package com.example.cqrswrite.domain.user.persistence;
 import com.example.cqrswrite.domain.user.model.User;
 import com.example.cqrswrite.domain.user.persistence.mapper.UserMapper;
 import com.example.cqrswrite.domain.user.persistence.repository.UserJpaRepository;
-import com.example.cqrswrite.domain.user.spi.UserSpi;
+import com.example.cqrswrite.domain.user.spi.UserPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class UserPersistenceAdapter implements UserSpi {
+public class UserPersistenceAdapter implements UserPort {
 
     private final UserJpaRepository userJpaRepository;
     private final UserMapper userMapper;
