b284ddc25ab994089f81502cc35adc3503e433f4
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-23 22:22:52 +0900

    ⚙️ :: @RequestBody

--- CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/domain/user/presentation/UserWebAdapter.java
+++ CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/domain/user/presentation/UserWebAdapter.java
@@ -4,11 +4,15 @@ import com.example.cqrswrite.domain.user.dto.request.CreateUserRequest;
 import com.example.cqrswrite.domain.user.presentation.dto.request.CreateUserWebRequest;
 import com.example.cqrswrite.domain.user.usecase.CreateUserUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -19,7 +23,10 @@ public class UserWebAdapter {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
-    public void createUser(CreateUserWebRequest request) {
+    public void createUser(@RequestBody @Valid CreateUserWebRequest request) {
+        System.out.println(request.getName());
+        System.out.println(request.getAccountId());
+        System.out.println(request.getPassword());
         createUserUseCase.execute(
                 CreateUserRequest.builder()
                         .name(request.getName())
