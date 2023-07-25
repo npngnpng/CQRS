4cfd8b64380c912a20e7059a5635ef8d6b944f69
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-25 20:43:21 +0900

    🗂️ :: kafka config

---
+++ CQRS-write/cqrswrite-infrastructure/src/main/java/com/example/cqrswrite/common/config/kafka/KafkaTopicConfig.java
@@ -0,0 +1,24 @@
+package com.example.cqrswrite.common.config.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic userTopic() {
+        return TopicBuilder
+                .name("user")
+                .build();
+    }
+
+    @Bean
+    public NewTopic feedTopic() {
+        return TopicBuilder
+                .name("feed")
+                .build();
+    }
+}
