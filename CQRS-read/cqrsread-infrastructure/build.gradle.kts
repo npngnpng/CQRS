2b500e8ddf392c882d34ac8c7647e798dfaca4fc
gilgeunwoo <kesksjxhx@gmail.com>
2023-07-26 21:50:37 +0900

    🛠️ :: 몽고디비로 교체

--- CQRS-read/cqrsread-infrastructure/build.gradle.kts
+++ CQRS-read/cqrsread-infrastructure/build.gradle.kts
@@ -19,8 +19,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation ("org.springframework.boot:spring-boot-starter-data-mongodb-reactive:3.1.2")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
