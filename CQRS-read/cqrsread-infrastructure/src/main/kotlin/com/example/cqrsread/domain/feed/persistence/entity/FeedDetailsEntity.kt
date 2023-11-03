545ef115122b4a601a9da373e90945eb38850266
gilgeunwoo <kesksjxhx@gmail.com>
2023-11-03 22:46:20 +0900

    🛩️ :: unused import

--- CQRS-read/cqrsread-infrastructure/src/main/kotlin/com/example/cqrsread/domain/feed/persistence/entity/FeedDetailsEntity.kt
+++ CQRS-read/cqrsread-infrastructure/src/main/kotlin/com/example/cqrsread/domain/feed/persistence/entity/FeedDetailsEntity.kt
@@ -3,7 +3,6 @@ package com.example.cqrsread.domain.feed.persistence.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
-import java.util.UUID
 
 @Document("feed_details")
 class FeedDetailsEntity(
--- CQRS-read/cqrsread-infrastructure/src/main/kotlin/com/example/cqrsread/domain/feed/persistence/repository/FeedDetailsRepository.kt
+++ CQRS-read/cqrsread-infrastructure/src/main/kotlin/com/example/cqrsread/domain/feed/persistence/repository/FeedDetailsRepository.kt
@@ -2,9 +2,6 @@ package com.example.cqrsread.domain.feed.persistence.repository
 
 import com.example.cqrsread.domain.feed.persistence.entity.FeedDetailsEntity
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import java.util.UUID
 
 interface FeedDetailsRepository: ReactiveMongoRepository<FeedDetailsEntity, String> {
-
-
 }
