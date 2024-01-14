081b8861d5822cd502a9b938c536722e26e4cfa9
길근우 <kesksjxhx@gmail.com>
2024-01-14 11:00:57 +0900

    🛠️ :: exception private

--- CQRS-write/cqrswrite-application/src/main/java/com/example/cqrswrite/domain/user/exception/UserException.java
+++ CQRS-write/cqrswrite-application/src/main/java/com/example/cqrswrite/domain/user/exception/UserException.java
@@ -11,7 +11,7 @@ public class UserException extends BaseException {
     public static final BaseException USER_NOT_FOUND = new UserException(UserErrorCode.USER_NOT_FOUND);
     public static final BaseException INVALID_PASSWORD = new UserException(UserErrorCode.INVALID_PASSWORD);
 
-    public UserException(ErrorProperty errorProperty) {
+    private UserException(ErrorProperty errorProperty) {
         super(errorProperty);
     }
 }
